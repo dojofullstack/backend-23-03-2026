c1d4f6f0c9c366d96d5fe02783b1199f1d47c293
dojofullstack <hola@dojofullstack.com>
2026-03-23 22:07:18 -0500

    update

--- rappi_ecommerce/settings.py
+++ rappi_ecommerce/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS',
-    'localhost,127.0.0.1',
+    'localhost,127.0.0.1,backend-23-03-2026.onrender.com',
 ).split(',')
 
 
